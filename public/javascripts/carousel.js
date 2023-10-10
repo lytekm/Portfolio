5b335d67c452bfaa959dd51574c3b328bcb6a6a2
lytekm <kevin.morrison653@gmail.com>
2023-10-10 06:04:12 -0400

    added about page and design updates

---
+++ public/javascripts/carousel.js
@@ -0,0 +1,17 @@
+const slide = document.querySelector(".carousel-slide");
+const images = document.querySelectorAll(".carousel-slide img");
+
+let currentIndex = 0;
+
+function showImage(index) {
+  // Hide all images
+  images.forEach((img) => {
+    img.style.transform = `translateX(-${index * 100}%)`;
+  });
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+}
+setInterval(nextImage, 3000); // Auto slide every 3 seconds
